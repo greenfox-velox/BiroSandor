f2c9799ee4b035d4ec9efb6966d117a488f8364a
Bíró Sándor <Br Sndor>
2016-05-23 16:57:13 +0200

    Decrypts exercise 1, 2

--- week-04/day-01/1-pip-io-modules/01_io/crypto_1dup.py
+++ week-04/day-01/1-pip-io-modules/01_io/crypto_1dup.py
@@ -1,4 +1,10 @@
 # Create a method that decrypts the texts/duplicated_chars.txt
 
 def decrypt(file_name):
-    pass
+    f = open(file_name)
+    output = ''
+    for  i in f:
+        for x in range(0, len(i), 2):
+            output += i[x]
+    f.close()
+    return output
--- week-04/day-01/1-pip-io-modules/01_io/crypto_2revlines.py
+++ week-04/day-01/1-pip-io-modules/01_io/crypto_2revlines.py
@@ -1,3 +1,14 @@
 # Create a method that decrypts texts/reversed_zen_lines.txt
 def decrypt(file_name):
-    pass
+    f = open(file_name)
+    result = f.readlines()
+    output = ''
+    for i in result:
+        temp = i.rstrip()
+        temp = temp[::-1]
+        temp = temp + '\n'
+        output += temp
+
+    f.close()
+
+    return output
