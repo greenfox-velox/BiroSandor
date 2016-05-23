770bfea2635b1381b6d3d5e2dc02eaa96beb99a2
Bíró Sándor <Br Sndor>
2016-05-23 19:39:06 +0200

    Crypto 3 revorded

--- week-04/day-01/1-pip-io-modules/01_io/crypto_3revorder.py
+++ week-04/day-01/1-pip-io-modules/01_io/crypto_3revorder.py
@@ -1,3 +1,8 @@
 # Create a method that decrypts texts/reversed_zen_order.txt
 def decrypt(file_name):
-    pass
+    f = open(file_name)
+    result = f.readlines()
+    temp = result[::-1]
+    output = "".join(temp)
+    f.close()
+    return output
