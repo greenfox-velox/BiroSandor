e9ab9fc2d1d185f45c3f8e1bcbf3660fe4851434
Bíró Sándor <Br Sndor>
2016-05-26 21:07:55 +0200

    fractal exercise done

--- week-04/day-04/11.py
+++ week-04/day-04/11.py
@@ -3,10 +3,10 @@ from tkinter import *
 
 root = Tk()
 
-canvas = Canvas(root, width=300, height=300, bg= 'yellow')
+canvas = Canvas(root, width=600, height=600, bg= 'yellow')
 canvas.pack()
-width = 300
-size = width // 3
+# width = 300
+# size = width // 3
 
 #
 # for i in range(3):
@@ -21,39 +21,75 @@ size = width // 3
 #                     y = j * size // 3
 #                     canvas.create_rectangle(x, y, x+size//3, y+size//3)
 
-def fractal():
+# def fractal():
+#
+#     canvas.create_rectangle(0, 0, 100, 100)
+#     canvas.create_rectangle(100, 0, 200, 100)
+#     canvas.create_rectangle(200, 0, 300, 100)
+#
+#     canvas.create_rectangle(0, 100, 100, 200)
+#     canvas.create_rectangle(100, 100, 200, 200)
+#     canvas.create_rectangle(200, 100, 300, 200)
+#
+#     canvas.create_rectangle(0, 200, 100, 300)
+#     canvas.create_rectangle(100, 200, 200, 300)
+#     canvas.create_rectangle(200, 200, 300, 300)
+#
+#
+#
+#
+#     canvas.create_rectangle(0, 100, 100/3, 100+100/3)
+#     canvas.create_rectangle(100/3, 100, 2*100/3, 100+100/3)
+#     canvas.create_rectangle(2*100/3, 100, 3*100/3, 100+100/3)
+#
+#     canvas.create_rectangle(0, 100+100/3, 100/3, 100+2*100/3)
+#     canvas.create_rectangle(100/3, 100+100/3, 2*100/3, 100+2*100/3)
+#     canvas.create_rectangle(2*100/3, 100+100/3, 100, 100+2*100/3)
+#
+#     canvas.create_rectangle(0, 100+2*100/3, 100/3, 2*100)
+#     canvas.create_rectangle(100/3, 100+2*100/3, 2*100/3, 100+100)
+#     canvas.create_rectangle(2*100/3, 100+2*100/3, 100, 2*100)
+#
+#
+#
+#
+#
+# fractal()
 
-    canvas.create_rectangle(0, 0, 100, 100)
-    canvas.create_rectangle(100, 0, 200, 100)
-    canvas.create_rectangle(200, 0, 300, 100)
+def fractal(x, y, size):
+    canvas.create_rectangle(x, y, size/3, size/3)
+    canvas.create_rectangle(x+size/3, y, 2*(size/3), size/3)
+    canvas.create_rectangle(2*(size/3), y, size, size/3)
 
-    canvas.create_rectangle(0, 100, 100, 200)
-    canvas.create_rectangle(100, 100, 200, 200)
-    canvas.create_rectangle(200, 100, 300, 200)
+    canvas.create_rectangle(x, y+size/3, size/3, 2*size/3)
+    canvas.create_rectangle(x+size/3, y+size/3, 2*(size/3), 2*(size/3))
+    canvas.create_rectangle(x+2*(size/3), y+size/3, size, 2*(size/3))
 
-    canvas.create_rectangle(0, 200, 100, 300)
-    canvas.create_rectangle(100, 200, 200, 300)
-    canvas.create_rectangle(200, 200, 300, 300)
-
-
+    canvas.create_rectangle(x, y+2*(size/3), size/3, size)
+    canvas.create_rectangle(x+size/3, y+2*(size/3), 2*(size/3), size)
+    canvas.create_rectangle(x+2*(size/3), y+2*(size/3), size, size)
 
 
-    canvas.create_rectangle(0, 100, 100/3, 100+100/3)
-    canvas.create_rectangle(100/3, 100, 2*100/3, 100+100/3)
-    canvas.create_rectangle(2*100/3, 100, 3*100/3, 100+100/3)
+    if size < 100:
+        pass
+    else:
+        fractal(x, y, size/3)
+        # fractal(x+100, y+100, size/3)
+        # fractal(x+size/3, y+size/3, size/3)
 
-    canvas.create_rectangle(0, 100+100/3, 100/3, 100+2*100/3)
-    canvas.create_rectangle(100/3, 100+100/3, 2*100/3, 100+2*100/3)
-    canvas.create_rectangle(2*100/3, 100+100/3, 100, 100+2*100/3)
-
-    canvas.create_rectangle(0, 100+2*100/3, 100/3, 2*100)
-    canvas.create_rectangle(100/3, 100+2*100/3, 2*100/3, 100+100)
-    canvas.create_rectangle(2*100/3, 100+2*100/3, 100, 2*100)
+fractal(0, 0, 300)
 
 
 
 
 
-fractal()
+
+
+
+
+
+
+
+
 
 root.mainloop()
---
+++ week-04/day-04/fractal2.py
@@ -0,0 +1,46 @@
+from tkinter import *
+
+root = Tk()
+canvas = Canvas(root, width=600, height=600)
+canvas.pack()
+
+# def fractal(x, y, size):
+#
+#
+#     canvas.create_rectangle(x+size/3, y, x+2*(size/3), y+size/3)
+#     canvas.create_rectangle(x, y+size/3, x+size/3, y+2*(size/3))
+#     canvas.create_rectangle(x+size/3, y+size/3, x+2*(size/3), y+2*(size/3))
+#     canvas.create_rectangle(x+2*(size/3), y+size/3, x+size, y+2*(size/3))
+#     canvas.create_rectangle(x+size/3, y+2*(size/3), x+2*(size/3), y+size)
+#
+#     if size < 3:
+#         pass
+#     else:
+#         fractal(x, y, size/3)
+#         fractal(x+200, y, size/3)
+#         fractal(x+400, y+200, size/3)
+#
+# fractal(0, 0, 600)
+
+
+def fractal(x, y, size):
+    canvas.create_rectangle(x + size, y, x + 2 * size, y + size)
+    canvas.create_rectangle(x, y + size, x + size, y + 2 * size)
+    canvas.create_rectangle(x + size, y + size, x + 2 * size, y + 2 * size)
+    canvas.create_rectangle(x + 2 * size, y + size, x + 3 * size, y + 2 * size)
+    canvas.create_rectangle(x + size, y + 2 * size, x + 2 * size, y + 3 * size)
+
+    if size < 3:
+        pass
+    else:
+        fractal(x+size, y, size/3)
+        fractal(x, y + size, size/3)
+        fractal(x + 2 * size, y + size, size/3)
+        fractal(x +size, y + 2* size, size / 3)
+
+
+
+fractal(0, 0, 200)
+
+
+root.mainloop()
