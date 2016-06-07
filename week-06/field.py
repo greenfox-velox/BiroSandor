9a0997981bb7d1425b38c8d502345dc0f04d12f8
Biro Sandor <biro.sandor.8@gmail.com>
2016-06-07 10:48:19 +0200

    Field is done

---
+++ week-06/field.py
@@ -0,0 +1,35 @@
+from tkinter import *
+root = Tk()
+
+canvas = Canvas(root, width = 720, height = 720)
+canvas.pack()
+image_floor = PhotoImage(file='floor.png')
+image_wall = PhotoImage(file='wall.png')
+
+
+
+a = [[1,1,1,0,0,1,1,0,0,1],
+     [1,0,1,0,0,1,1,0,0,1],
+     [1,1,1,1,1,1,1,0,0,1],
+     [1,1,1,0,0,1,1,0,0,1],
+     [1,0,1,0,0,1,1,1,1,1],
+     [1,1,1,0,0,1,1,0,0,1],
+     [1,1,1,0,0,1,1,0,0,1],
+     [1,0,1,0,0,1,1,0,1,1],
+     [1,0,1,1,1,1,1,0,1,1],
+     [1,1,1,0,0,1,1,0,0,1]]
+
+def create_floor(x,y):
+    floor = canvas.create_image(x, y, image=image_floor, anchor=NW)
+
+def create_wall(x,y):
+    wall = canvas.create_image(x, y, image=image_wall, anchor=NW)
+
+for row_numbers in range(10): #'a' listan beluli listak szama, sorok szama
+    for column_numbers in range(10): #ahany eleme van a belso listanak, oszlopok szama
+        if a[row_numbers][column_numbers] == 1:
+            create_floor(column_numbers*72, row_numbers*72)
+        else:
+            create_wall(column_numbers*72,row_numbers*72)
+
+root.mainloop()
