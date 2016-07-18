88c31130a4cc916b9dcf00f809ad454cda93665f
Biro Sandor <biro.sandor.8@gmail.com>
2016-07-18 21:47:47 +0200

    refactor todo frontend

--- week-08/project_todo/todo.js
+++ week-08/project_todo/todo.js
@@ -5,106 +5,99 @@ buttonAdd.addEventListener('click', addNewTodo);
 
 var item = document.querySelector('ul');
 
-function createOneNewTodo(todo) {
- var newItem = document.createElement('li');
- newItem.textContent = todo.text;
- newItem.setAttribute('id', todo.id);
- item.appendChild(newItem);
- createDelButton(newItem);
- createDoneButton(newItem, todo);
+// var url = 'https://mysterious-dusk-8248.herokuapp.com/todos'
+var local = 'http://localhost:3000/todos'
+var input_text = document.querySelector('.input_text');
+
+function httpRequest (method, url, data, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(data);
+  xhr.onload = function() {
+  if (xhr.readyState === xhr.DONE) {
+    var response = JSON.parse(xhr.response);
+    callback(response)};
+  };
 }
 
-function createDelButton (parent) {
- var del = document.createElement('button');
- del.classList.add('del-button');
- del.setAttribute('id', parent.id);
- parent.appendChild(del);
- del.addEventListener('click', function(event){
-   delTodo(event);
- });
+function onLoad () {
+  httpRequest ('GET', local, '', function (response){
+    listTheTodos(response);
+  });
 }
 
-function createDoneButton (parent, todo) {
- var done = document.createElement('button');
- if (todo.completed) {
-  done.classList.add('true');
- } else {
-  done.classList.add('done-button');
- }
- done.setAttribute('id', parent.id);
- parent.appendChild(done);
- done.addEventListener('click', function(event){
-   doneTodo(event);
- });
+function addNewTodo () {
+  httpRequest ('POST', local, JSON.stringify({text: input_text.value}), function (response) {
+    createOneNewTodo(response);
+    clear();
+  });
+}
+
+function delTodo (event) {
+  var id = event.target.parentNode.id;
+  httpRequest ('DELETE', local + '/' + id, JSON.stringify({destroyed: true}), function (response) {
+    document.getElementById(id).remove();
+  });
+}
+
+function doneTodo (event) {
+  var id = event.target.parentNode.id;
+  httpRequest ('PUT', local + '/' + id, JSON.stringify({text: text, completed: true}),function () {
+    event.target.classList.add('true');
+  });
 }
 
 function listTheTodos (todo) {
   todo.forEach(function (e) {
     createOneNewTodo(e);
-  })};
+  });
+}
 
+function createOneNewTodo(todo) {
+  var newItem = document.createElement('li');
+  newItem.textContent = todo.text;
+  newItem.setAttribute('id', todo.id);
+  item.appendChild(newItem);
+  createDelButton(newItem);
+  createDoneButton(newItem, todo);
+}
 
-function onLoad () {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://mysterious-dusk-8248.herokuapp.com/todos');
-  xhr.onreadystatechange = function() {
-     if (xhr.readyState == 4) {
-       var response = JSON.parse(xhr.response);
-       listTheTodos(response)};
-     };
-  xhr.send();
-};
+function createDelButton (parent) {
+  var del = document.createElement('button');
+  del.classList.add('del-button');
+  del.setAttribute('id', parent.id);
+  parent.appendChild(del);
+  del.addEventListener('click', function(event){
+    delTodo(event);
+  });
+}
 
-onLoad();
+function createDoneButton (parent, todo) {
+  var done = document.createElement('button');
+  if (todo.completed) {
+    done.classList.add('true');
+  } else {
+    done.classList.add('done-button');
+  }
+  done.setAttribute('id', parent.id);
+  parent.appendChild(done);
+  done.addEventListener('click', function(event){
+  doneTodo(event);
+  });
+}
 
 function reload () {
   var container = document.querySelector('ul');
   container.innerHTML = '';
   onLoad();
 };
+
 setInterval(reload, 10000);
 
-function addNewTodo () {
-  var input_text = document.querySelector('.input_text');
-  var text = input_text.value;
-  var newTodo = JSON.stringify({text: text});
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://mysterious-dusk-8248.herokuapp.com/todos');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onload = function() {
-     if (xhr.readyState == 4) {
-      var response = JSON.parse(xhr.response);
-      createOneNewTodo(response);
-     };
-    };
-  xhr.send(newTodo);
-};
+function clear() {
+  var content = document.querySelector('.input_text');
+  content.value = '';
+}
 
-function delTodo (event) {
-  var xhr = new XMLHttpRequest();
-  var id = event.target.parentNode.id;
-  xhr.open('DELETE', 'https://mysterious-dusk-8248.herokuapp.com/todos' + '/' + id);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onload = function() {
-     if (xhr.readyState == 4) {
-      document.getElementById(id).remove();
-     };
-   };
-   xhr.send();
-};
-
-function doneTodo (event) {
-  var xhr = new XMLHttpRequest();
-  var id = event.target.parentNode.id;
-  var text = event.target.parentNode.textContent;
-  var result = JSON.stringify({text: text, completed: true});
-  xhr.open('PUT', 'https://mysterious-dusk-8248.herokuapp.com/todos' + '/' + id);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onload = function() {
-      if (xhr.readyState == 4) {
-        var response = JSON.parse(xhr.response);
-        event.target.classList.add('true');
-      }
-    };
-  xhr.send(result);
-};
+onLoad();
